05a529b175c62f105bd7dec61b1fc1243848d148
Robert Killion <robertfkillion@gmail.com>
2021-05-05 23:06:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase===string) {
+  if (whisper(string)===string) {
     return "I can't hear you!";
   } else if (shout(string)===string) {
     return "YES INDEED!";
